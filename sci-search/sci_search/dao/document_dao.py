908c778ecb9958289d4b044bdd2687c942b7051d
Viktor <viktor2karlsson@gmail.com>
2020-12-10 07:22:37 +0100

    enable update of pages

--- sci-search/sci_search/dao/document_dao.py
+++ sci-search/sci_search/dao/document_dao.py
@@ -7,19 +7,16 @@ from typing import List
 
 class DocumentDAO:
 
-    # Create    [x]
-    # Read      [x]
-    # Update    [ ]
-    # Delete    [ ]
-
     def create_doc(self, db: Session, doc: schemas.DocumentCreate) -> schemas.Document:
-        pages = [models.Page(page_number=page.page_number, content=page.content) for page in doc.pages]
-        fields = [models.Field(name=field.name, value=field.value) for field in doc.doc_fields]
-
-        db_doc = models.Document(title=doc.title, pages=pages, fields=fields)
+        db_doc = models.Document(title=doc.title)
         db.add(db_doc)
         db.commit()
         db.refresh(db_doc)
+        doc_id = db_doc.id
+
+        self.set_fields_for_document(db, doc_id, doc.doc_fields)
+        self.set_pages_for_document(db, doc_id, doc.pages)
+
         return db_doc
 
     def get_docs(self, db: Session, skip, limit) -> List[schemas.Document]:
@@ -50,22 +47,47 @@ class DocumentDAO:
         db.commit()
         db.refresh(db_field)
         return db_field
+
+    def set_pages_for_document(self, db: Session, doc_id: int, pages: List[schemas.PageBase]) -> schemas.Document:
+        for p in pages:
+            self.set_page_for_document(db, doc_id, p)
+
+        return self.get_doc_from_id(db, doc_id)
+
+    def set_page_for_document(self, db: Session, doc_id: int, page: schemas.PageBase) -> schemas.Page:
+        db_page = models.Page(**page.dict(), document_id=doc_id)
+        db.add(db_page)
+        db.commit()
+        db.refresh(db_page)
+        return db_page
     
     def replace_fields_for_document(self, db: Session, doc_id: int, fields: List[schemas.FieldBase]) -> schemas.Document:
         self.delete_document_fields(db, doc_id)
         return self.set_fields_for_document(db, doc_id, fields)
     
+    def replace_pages_for_document(self, db: Session, doc_id: int, pages: List[schemas.PageBase]):
+        self.delete_document_pages(db, doc_id)
+        return self.set_pages_for_document(db, doc_id, pages)
+
     def delete_document_fields(self, db: Session, doc_id: int):
         db_fields = self.get_doc_from_id(db, doc_id).doc_fields
-
         for f in db_fields:
             db.delete(f)
-        
         db.commit()
+
+    def delete_document_pages(self, db: Session, doc_id: int):
+        db_pages = self.get_doc_from_id(db, doc_id).pages
+        for p in db_pages:
+            db.delete(p)
+        db.commit()
+
 
     def delete_doc(self, db: Session, doc_id: int) -> schemas.Document:
         db_doc = self.get_doc_from_id(db, doc_id)
         db.delete(db_doc)
         db.commit()
         return db_doc
+
+
+
 
--- sci-search/sci_search/main.py
+++ sci-search/sci_search/main.py
@@ -31,7 +31,7 @@ def get_db():
 
 
 @app.post("/docs/", response_model=schemas.Document)
-def create_doc(doc: schemas.DocumentBase, db: Session = Depends(get_db)):
+def create_doc(doc: schemas.DocumentCreate, db: Session = Depends(get_db)):
     return document_dao.create_doc(db, doc)
 
 @app.get("/docs/", response_model=List[schemas.Document])
@@ -50,9 +50,17 @@ def set_doc_title(doc_id: int, title: str, db: Session = Depends(get_db)):
 def set_doc_fields(doc_id: int, fields: List[schemas.FieldBase], db: Session = Depends(get_db)):
     return document_dao.set_fields_for_document(db, doc_id, fields)
 
+@app.post("/docs/{doc_id}/pages", response_model=schemas.Document)
+def set_doc_pages(doc_id: int, pages: List[schemas.PageBase], db: Session = Depends(get_db)):
+    return document_dao.set_pages_for_document(db, doc_id, pages)
+
 @app.put("/docs/{doc_id}/fields", response_model=schemas.Document)
 def replace_doc_fields(doc_id: int, fields: List[schemas.FieldBase], db: Session = Depends(get_db)):
     return document_dao.replace_fields_for_document(db, doc_id, fields)
+
+@app.put("/docs/{doc_id}/pages", response_model=schemas.Document)
+def replace_doc_pages(doc_id: int, pages: List[schemas.PageBase], db: Session = Depends(get_db)):
+    return document_dao.replace_pages_for_document(db, doc_id, pages)
 
 @app.delete("/docs/{doc_id}", response_model=schemas.Document)
 def delete_doc(doc_id: int, db: Session = Depends(get_db)):
