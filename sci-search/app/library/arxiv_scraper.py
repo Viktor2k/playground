8310841babfcd7b4e2243d2cd2680fe9a26d2244
Viktor <viktor2karlsson@gmail.com>
2020-12-15 21:05:20 +0100

    add functionallity for extracting docs and metadata from a particular day

--- sci-search/app/library/arxiv_scraper.py
+++ sci-search/app/library/arxiv_scraper.py
@@ -1,4 +1,5 @@
 import os
+import json
 import arxiv
 import argparse
 
@@ -8,6 +9,7 @@ from typing import List
 from tqdm import tqdm
 
 ARXIV_DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+CLI_DATE_FORMAT = "%Y-%m-%d"
 DEFAULT_ROOT_DIRECTORY = "/Users/Viktor/git/playground/sci-search/arxiv-files"
 DOCUMENT_DATE_NAME = "published"
 
@@ -45,7 +47,13 @@ def set_default_time_units(date: datetime) -> datetime:
 
 def download_documents(docs: List[dict], root_path: str):
     for d in tqdm(docs, desc = f"Downloading {len(docs)} documents to path {os.path.abspath(root_path)}"):
-        arxiv.arxiv.download(d, get_storage_path(root_path, d))
+        arxiv.arxiv.download(d, get_storage_path(root_path, d), slugify=get_file_name)
+        d["file_path"] = f"{get_storage_path(root_path, d)}/{get_file_name(d)}"
+
+    with open(f"{root_path}/metadata.json", "a+") as metadata_file:
+        for d in docs:
+            metadata_file.write(json.dumps(d) + "\n")
+
 
 def get_storage_path(root_path, doc: dict) -> str:
     date = parse_arxiv_date(doc[DOCUMENT_DATE_NAME])
@@ -54,6 +62,9 @@ def get_storage_path(root_path, doc: dict) -> str:
         Path(dir_path).mkdir(parents=True)
 
     return dir_path
+
+def get_file_name(doc: dict) -> str:
+    return doc["id"].split("/")[-1]
 
 def get_n_previous_days(n: int) -> List[datetime]:
     base = datetime.today()
@@ -74,7 +85,8 @@ parser = argparse.ArgumentParser(description="Download arxiv files last n days")
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-n", "--n_days", metavar="days", type=int, help="Number of days in the past to fetch documents from.")
-group.add_argument("-r", "--range", metavar=("date_start", "date_end"), type=str, nargs=2, help="Range of days to extract documents from. Has to be on format 'Y-M-D'")
+group.add_argument("-d", "--date", metavar="date", type=str, help="Date to extract documents from. Has to be on format 'Y-M-D'")
+group.add_argument("-r", "--range", metavar=("date_start", "date_end"), type=str, nargs=2, help=f"Range of days to extract documents from. Has to be on format '{CLI_DATE_FORMAT}'")
 
 parser.add_argument("-p", "--path", metavar="path", type=str, help="Root path to store exported files in", default=DEFAULT_ROOT_DIRECTORY)
 
@@ -82,9 +94,15 @@ args = parser.parse_args()
 if args.n_days:
     date_range = get_n_previous_days(args.n_days)
 elif args.range:
-    start = datetime.strptime(args.range[0], "%Y-%m-%d")
-    end = datetime.strptime(args.range[1], "%Y-%m-%d")
+    start = datetime.strptime(args.range[0], CLI_DATE_FORMAT)
+    end = datetime.strptime(args.range[1], CLI_DATE_FORMAT)
     date_range = get_dates_in_range(start, end)
+elif args.date:
+    date_range = [datetime.strptime(args.date, CLI_DATE_FORMAT)]
 
-print(f"Extracting documents from date range {prettify_dates(date_range)}...")
+if len(date_range) == 1:
+    print(f"Extracting documents from date {prettify_dates(date_range)}")
+else:
+    print(f"Extracting documents from date range {prettify_dates(date_range)}...")
+
 download_documents_from_dates_to_path(date_range, args.path)
